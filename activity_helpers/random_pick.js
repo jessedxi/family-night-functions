829b2424ff5ba89e98ced46b25afad8564313a8e
Jesse <jessedaniel@live.ca>
2021-06-11 11:22:24 -0600

    add minimist to process argv

--- activity_helpers/random_pick.js
+++ activity_helpers/random_pick.js
@@ -1,3 +1,9 @@
+const parseArgs = require("minimist");
+
+const args = parseArgs(process.argv.slice(2));
+
 function getRandom(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
+
+console.log(getRandom(args.variable));
